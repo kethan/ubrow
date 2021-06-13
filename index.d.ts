4e6913722588810d944992df7afd5c5179d9b7fb
Kethan <kchandrakanth@thapovan-inc.com>
2021-06-13 22:09:30 +0530

    export typings from urout

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-import { Router, IOptions, Request } from "urout";
+import { Router, IOptions, Request, Response, Middleware, ErrorHandler, NextHandler, ParsedURL, IError, Pattern, Promisable } from "urout";
 interface ClientOpts extends IOptions {
     base?: string;
 }
@@ -9,4 +9,7 @@ declare class UBrow extends Router {
     private on(event: string, callback: (location: Location, req: Request, stateType: string) => {}): this;
     navigate(url: string): void;
 }
+
 export declare function Client({ onError, onNoMatch, base }?: ClientOpts): UBrow;
+
+export { Request, Response, Middleware, ErrorHandler, NextHandler, ParsedURL, IError, Pattern, Promisable }
